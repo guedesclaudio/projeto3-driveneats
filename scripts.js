0710cb31f2eedb6f36176deb46d8ef4e6564f8fd
guedesclaudio <cgsarmentosilva@gmail.com>
2022-06-08 15:53:24 -0300

    Sexto commit

--- scripts.js
+++ scripts.js
@@ -1,99 +1,184 @@
-var x;
-variaveis = []
+let prato = 0
+let bebida = 0
+let sobremesa = 0
+let precoComida = 0
+let precoBebida = 0
+let precoSobremesa = 0
+
+function mudaBotao () {
+    if (prato != 0 && bebida != 0  && sobremesa != 0 ) {
+        console.log('passando')
+        document.getElementById('botao-envia').style.background = "#32B72F"
+        document.getElementById('texto-botao').innerHTML = '<h1 onclick = "enviaWpp()">Fechar pedido</h1>'
+    }
+}
+
+function calculaPreco () {
+    total = precoComida + precoBebida + precoSobremesa
+    console.log(total)
+    return total
+}
+
+function enviaWpp() {
+    nome = prompt('Qual o seu nome?')
+    endereco = prompt('Qual o seu endereço?')
+    var str = `
+    Olá, gostaria de fazer o pedido:
+    - Prato: ${prato}
+    - Bebida: ${bebida}
+    - Sobremesa: ${sobremesa}
+    Total: R$ ${total.toFixed(2)}
+
+    Nome: ${nome}
+    Endereço: ${endereco}
+    `
+    texto = encodeURIComponent(str);
+    var link = `https://api.whatsapp.com/send?phone=5521999570191&text=${texto}`;
+
+    setTimeout(function() {window.open(link)}, 1000)
+  }
 
 function selecionaItem1 () {
-    document.getElementById('prato1').style.border = "4px solid green"
+    document.getElementById('prato1').style.border = "4px solid #32B72F"
     document.getElementById('prato2').style.border = "none"
     document.getElementById('prato3').style.border = "none"
     document.getElementById('select-img1').style.display = "initial"
     document.getElementById('select-img2').style.display = "none"
     document.getElementById('select-img3').style.display = "none"
+
+    prato = 'Frango Yin Yang'
+    precoComida = 14.90
+
+    mudaBotao()
+    calculaPreco()
 }
 
-console.log(variaveis)
 
 function selecionaItem2 () {
     document.getElementById('prato1').style.border = "none"
-    document.getElementById('prato2').style.border = "4px solid green"
+    document.getElementById('prato2').style.border = "4px solid #32B72F"
     document.getElementById('prato3').style.border = "none"
     document.getElementById('select-img1').style.display = "none"
     document.getElementById('select-img2').style.display = "initial"
     document.getElementById('select-img3').style.display = "none"
+
+    prato = 'Frango Yin Yang'
+    precoComida = 14.90
+
+    mudaBotao()
+    calculaPreco()
 }
 
 function selecionaItem3 () {
     document.getElementById('prato1').style.border = "none"
     document.getElementById('prato2').style.border = "none"
-    document.getElementById('prato3').style.border = "4px solid green"
+    document.getElementById('prato3').style.border = "4px solid #32B72F"
     document.getElementById('select-img1').style.display = "none"
     document.getElementById('select-img2').style.display = "none"
     document.getElementById('select-img3').style.display = "initial"
-    
+
+    prato = 'Frango Yin Yang'
+    precoComida = 14.90
+
+    mudaBotao()
+    calculaPreco()
 }
 
 function selecionaBebida1 () {
-    document.getElementById('bebida1').style.border = "4px solid green"
+    document.getElementById('bebida1').style.border = "4px solid #32B72F"
     document.getElementById('bebida2').style.border = "none"
     document.getElementById('bebida3').style.border = "none"
     document.getElementById('select-bebida1').style.display = "initial"
     document.getElementById('select-bebida2').style.display = "none"
     document.getElementById('select-bebida3').style.display = "none"
     y = 1
+
+    bebida = 'Coquinha Gelada'
+    precoBebida = 4.90
+
+    mudaBotao()
+    calculaPreco()
 }
 
 function selecionaBebida2 () {
     document.getElementById('bebida1').style.border = "none"
-    document.getElementById('bebida2').style.border = "4px solid green"
+    document.getElementById('bebida2').style.border = "4px solid #32B72F"
     document.getElementById('bebida3').style.border = "none"
     document.getElementById('select-bebida1').style.display = "none"
     document.getElementById('select-bebida2').style.display = "initial"
     document.getElementById('select-bebida3').style.display = "none"
     y = 1
+
+    bebida = 'Coquinha Gelada'
+    precoBebida = 4.90
+
+    mudaBotao()
+    calculaPreco()
 }
 
 function selecionaBebida3 () {
     document.getElementById('bebida1').style.border = "none"
     document.getElementById('bebida2').style.border = "none"
-    document.getElementById('bebida3').style.border = "4px solid green"
+    document.getElementById('bebida3').style.border = "4px solid #32B72F"
     document.getElementById('select-bebida1').style.display = "none"
     document.getElementById('select-bebida2').style.display = "none"
     document.getElementById('select-bebida3').style.display = "initial"
     y = 1
+
+    bebida = 'Coquinha Gelada'
+    precoBebida = 4.90
+
+    mudaBotao()
+    calculaPreco()
 }
 
 function selecionaSobremesa1 () {
-    document.getElementById('sobremesa1').style.border = "4px solid green"
+    document.getElementById('sobremesa1').style.border = "4px solid #32B72F"
     document.getElementById('sobremesa2').style.border = "none"
     document.getElementById('sobremesa3').style.border = "none"
     document.getElementById('select-sobremesa1').style.display = "initial"
     document.getElementById('select-sobremesa2').style.display = "none"
     document.getElementById('select-sobremesa3').style.display = "none"
     z = 1
+
+    sobremesa = 'Pudim'
+    precoSobremesa = 7.90
+
+    mudaBotao()
+    calculaPreco()
 }
 
 function selecionaSobremesa2 () {
     document.getElementById('sobremesa1').style.border = "none"
-    document.getElementById('sobremesa2').style.border = "4px solid green"
+    document.getElementById('sobremesa2').style.border = "4px solid #32B72F"
     document.getElementById('sobremesa3').style.border = "none"
     document.getElementById('select-sobremesa1').style.display = "none"
     document.getElementById('select-sobremesa2').style.display = "initial"
     document.getElementById('select-sobremesa3').style.display = "none"
     z = 1
+
+    sobremesa = 'Pudim'
+    precoSobremesa = 7.90
+
+    mudaBotao()
+    calculaPreco()
 }
 
 function selecionaSobremesa3 () {
     document.getElementById('sobremesa1').style.border = "none"
     document.getElementById('sobremesa2').style.border = "none"
-    document.getElementById('sobremesa3').style.border = "4px solid green"
+    document.getElementById('sobremesa3').style.border = "4px solid #32B72F"
     document.getElementById('select-sobremesa1').style.display = "none"
     document.getElementById('select-sobremesa2').style.display = "none"
     document.getElementById('select-sobremesa3').style.display = "initial"
     z = 1
+
+    sobremesa = 'Pudim'
+    precoSobremesa = 7.90
+
+    mudaBotao()
+    calculaPreco()
 }
 
 
 
-if (true) {
-    console.log('passando')
-    document.getElementById('botao-envia').style.background = "green"
-}
