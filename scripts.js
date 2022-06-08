48811bc63a224ee279981d46f822c55726d8ab1d
guedesclaudio <cgsarmentosilva@gmail.com>
2022-06-08 17:48:06 -0300

    Setimo commit

--- scripts.js
+++ scripts.js
@@ -22,6 +22,10 @@ function calculaPreco () {
 function enviaWpp() {
     nome = prompt('Qual o seu nome?')
     endereco = prompt('Qual o seu endereço?')
+
+    document.getElementById('body').style.opacity = '0.3'
+    document.getElementById('confirma-pedido').innerHTML = '<div class="box-final"></div>'
+
     var str = `
     Olá, gostaria de fazer o pedido:
     - Prato: ${prato}
@@ -33,15 +37,16 @@ function enviaWpp() {
     Endereço: ${endereco}
     `
     texto = encodeURIComponent(str);
-    var link = `https://api.whatsapp.com/send?phone=5521999570191&text=${texto}`;
+    //var link = `https://api.whatsapp.com/send?phone=5521999570191&text=${texto}`;
 
-    setTimeout(function() {window.open(link)}, 1000)
+    //setTimeout(function() {window.open(link)}, 1000)
   }
 
 function selecionaItem1 () {
-    document.getElementById('prato1').style.border = "4px solid #32B72F"
-    document.getElementById('prato2').style.border = "none"
-    document.getElementById('prato3').style.border = "none"
+    document.getElementById('prato1').style.border = "3px solid #32B72F"
+    //document.getElementById('prato1').style.boxSizing = "border-box"
+    document.getElementById('prato2').style.border = "3px solid white"
+    document.getElementById('prato3').style.border = "3px solid white"
     document.getElementById('select-img1').style.display = "initial"
     document.getElementById('select-img2').style.display = "none"
     document.getElementById('select-img3').style.display = "none"
@@ -55,9 +60,9 @@ function selecionaItem1 () {
 
 
 function selecionaItem2 () {
-    document.getElementById('prato1').style.border = "none"
-    document.getElementById('prato2').style.border = "4px solid #32B72F"
-    document.getElementById('prato3').style.border = "none"
+    document.getElementById('prato1').style.border = "3px solid white"
+    document.getElementById('prato2').style.border = "3px solid #32B72F"
+    document.getElementById('prato3').style.border = "3px solid white"
     document.getElementById('select-img1').style.display = "none"
     document.getElementById('select-img2').style.display = "initial"
     document.getElementById('select-img3').style.display = "none"
@@ -70,9 +75,9 @@ function selecionaItem2 () {
 }
 
 function selecionaItem3 () {
-    document.getElementById('prato1').style.border = "none"
-    document.getElementById('prato2').style.border = "none"
-    document.getElementById('prato3').style.border = "4px solid #32B72F"
+    document.getElementById('prato1').style.border = "3px solid white"
+    document.getElementById('prato2').style.border = "3px solid white"
+    document.getElementById('prato3').style.border = "3px solid #32B72F"
     document.getElementById('select-img1').style.display = "none"
     document.getElementById('select-img2').style.display = "none"
     document.getElementById('select-img3').style.display = "initial"
@@ -85,9 +90,9 @@ function selecionaItem3 () {
 }
 
 function selecionaBebida1 () {
-    document.getElementById('bebida1').style.border = "4px solid #32B72F"
-    document.getElementById('bebida2').style.border = "none"
-    document.getElementById('bebida3').style.border = "none"
+    document.getElementById('bebida1').style.border = "3px solid #32B72F"
+    document.getElementById('bebida2').style.border = "3px solid white"
+    document.getElementById('bebida3').style.border = "3px solid white"
     document.getElementById('select-bebida1').style.display = "initial"
     document.getElementById('select-bebida2').style.display = "none"
     document.getElementById('select-bebida3').style.display = "none"
@@ -101,9 +106,9 @@ function selecionaBebida1 () {
 }
 
 function selecionaBebida2 () {
-    document.getElementById('bebida1').style.border = "none"
-    document.getElementById('bebida2').style.border = "4px solid #32B72F"
-    document.getElementById('bebida3').style.border = "none"
+    document.getElementById('bebida1').style.border = "3px solid white"
+    document.getElementById('bebida2').style.border = "3px solid #32B72F"
+    document.getElementById('bebida3').style.border = "3px solid white"
     document.getElementById('select-bebida1').style.display = "none"
     document.getElementById('select-bebida2').style.display = "initial"
     document.getElementById('select-bebida3').style.display = "none"
@@ -117,9 +122,9 @@ function selecionaBebida2 () {
 }
 
 function selecionaBebida3 () {
-    document.getElementById('bebida1').style.border = "none"
-    document.getElementById('bebida2').style.border = "none"
-    document.getElementById('bebida3').style.border = "4px solid #32B72F"
+    document.getElementById('bebida1').style.border = "3px solid white"
+    document.getElementById('bebida2').style.border = "3px solid white"
+    document.getElementById('bebida3').style.border = "3px solid #32B72F"
     document.getElementById('select-bebida1').style.display = "none"
     document.getElementById('select-bebida2').style.display = "none"
     document.getElementById('select-bebida3').style.display = "initial"
@@ -133,9 +138,9 @@ function selecionaBebida3 () {
 }
 
 function selecionaSobremesa1 () {
-    document.getElementById('sobremesa1').style.border = "4px solid #32B72F"
-    document.getElementById('sobremesa2').style.border = "none"
-    document.getElementById('sobremesa3').style.border = "none"
+    document.getElementById('sobremesa1').style.border = "3px solid #32B72F"
+    document.getElementById('sobremesa2').style.border = "3px solid white"
+    document.getElementById('sobremesa3').style.border = "3px solid white"
     document.getElementById('select-sobremesa1').style.display = "initial"
     document.getElementById('select-sobremesa2').style.display = "none"
     document.getElementById('select-sobremesa3').style.display = "none"
@@ -149,9 +154,9 @@ function selecionaSobremesa1 () {
 }
 
 function selecionaSobremesa2 () {
-    document.getElementById('sobremesa1').style.border = "none"
-    document.getElementById('sobremesa2').style.border = "4px solid #32B72F"
-    document.getElementById('sobremesa3').style.border = "none"
+    document.getElementById('sobremesa1').style.border = "3px solid white"
+    document.getElementById('sobremesa2').style.border = "3px solid #32B72F"
+    document.getElementById('sobremesa3').style.border = "3px solid white"
     document.getElementById('select-sobremesa1').style.display = "none"
     document.getElementById('select-sobremesa2').style.display = "initial"
     document.getElementById('select-sobremesa3').style.display = "none"
@@ -165,9 +170,9 @@ function selecionaSobremesa2 () {
 }
 
 function selecionaSobremesa3 () {
-    document.getElementById('sobremesa1').style.border = "none"
-    document.getElementById('sobremesa2').style.border = "none"
-    document.getElementById('sobremesa3').style.border = "4px solid #32B72F"
+    document.getElementById('sobremesa1').style.border = "3px solid white"
+    document.getElementById('sobremesa2').style.border = "3px solid white"
+    document.getElementById('sobremesa3').style.border = "3px solid #32B72F"
     document.getElementById('select-sobremesa1').style.display = "none"
     document.getElementById('select-sobremesa2').style.display = "none"
     document.getElementById('select-sobremesa3').style.display = "initial"
