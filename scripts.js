4d723e601e1a25413891a66cd5fbbd8a2e8c3a06
guedesclaudio <cgsarmentosilva@gmail.com>
2022-06-10 16:15:49 -0300

    Decimo commit

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ let precoBebida = 0
 let precoSobremesa = 0
 let nome = 0
 let endereco = 0
+let varia = true
 
 function mudaBotao () {
     if (prato != 0 && bebida != 0  && sobremesa != 0 ) {
@@ -38,6 +39,12 @@ function confirmaPedido() {
     <div class="alinha-botao"><button onclick="enviaWpp()">Tudo certo, pode pedir</button></div>
     <h4 onclick="cancelaPedido()">Cancelar</h4>
     `
+    if (document.querySelector('body').classList = "pausa-body") {
+        varia = false
+        console.log('chegou aqui')
+        console.log(varia)
+    }
+
 }
 
 function cancelaPedido() {
@@ -64,146 +71,200 @@ function enviaWpp() {
     setTimeout(function() {window.open(link)}, 1000)
   }
 
-function selecionaItem1 () {
-    document.getElementById('prato1').style.border = "3px solid #32B72F"
-    document.getElementById('prato2').style.border = "3px solid white"
-    document.getElementById('prato3').style.border = "3px solid white"
-    document.getElementById('select-img1').style.display = "initial"
-    document.getElementById('select-img2').style.display = "none"
-    document.getElementById('select-img3').style.display = "none"
+  
+  function selecionaItem1 () {
 
-    prato = 'Frango Yin Yang'
-    precoComida = 14.90
+    if (varia === true) {
 
-    mudaBotao()
-    calculaPreco()
+        document.getElementById('prato1').style.border = "3px solid #32B72F"
+        document.getElementById('prato2').style.border = "3px solid white"
+        document.getElementById('prato3').style.border = "3px solid white"
+        document.getElementById('select-img1').style.display = "initial"
+        document.getElementById('select-img2').style.display = "none"
+        document.getElementById('select-img3').style.display = "none"
+    
+        prato = 'Frango Yin Yang P'
+        precoComida = 14.90
+    
+        mudaBotao()
+        calculaPreco()
+
+    }
+
 }
 
 
 function selecionaItem2 () {
-    document.getElementById('prato1').style.border = "3px solid white"
-    document.getElementById('prato2').style.border = "3px solid #32B72F"
-    document.getElementById('prato3').style.border = "3px solid white"
-    document.getElementById('select-img1').style.display = "none"
-    document.getElementById('select-img2').style.display = "initial"
-    document.getElementById('select-img3').style.display = "none"
 
-    prato = 'Frango Yin Yang'
-    precoComida = 14.90
+    if (varia === true) {
 
-    mudaBotao()
-    calculaPreco()
+        document.getElementById('prato1').style.border = "3px solid white"
+        document.getElementById('prato2').style.border = "3px solid #32B72F"
+        document.getElementById('prato3').style.border = "3px solid white"
+        document.getElementById('select-img1').style.display = "none"
+        document.getElementById('select-img2').style.display = "initial"
+        document.getElementById('select-img3').style.display = "none"
+
+        prato = 'Frango Yin Yang M'
+        precoComida = 20.90
+
+        mudaBotao()
+        calculaPreco()
+
+    }
+    
 }
 
 function selecionaItem3 () {
-    document.getElementById('prato1').style.border = "3px solid white"
-    document.getElementById('prato2').style.border = "3px solid white"
-    document.getElementById('prato3').style.border = "3px solid #32B72F"
-    document.getElementById('select-img1').style.display = "none"
-    document.getElementById('select-img2').style.display = "none"
-    document.getElementById('select-img3').style.display = "initial"
 
-    prato = 'Frango Yin Yang'
-    precoComida = 14.90
+    if(varia === true) {
 
-    mudaBotao()
-    calculaPreco()
+        document.getElementById('prato1').style.border = "3px solid white"
+        document.getElementById('prato2').style.border = "3px solid white"
+        document.getElementById('prato3').style.border = "3px solid #32B72F"
+        document.getElementById('select-img1').style.display = "none"
+        document.getElementById('select-img2').style.display = "none"
+        document.getElementById('select-img3').style.display = "initial"
+
+        prato = 'Frango Yin Yang G'
+        precoComida = 24.90
+
+        mudaBotao()
+        calculaPreco()
+
+    }
+    
 }
 
 function selecionaBebida1 () {
-    document.getElementById('bebida1').style.border = "3px solid #32B72F"
-    document.getElementById('bebida2').style.border = "3px solid white"
-    document.getElementById('bebida3').style.border = "3px solid white"
-    document.getElementById('select-bebida1').style.display = "initial"
-    document.getElementById('select-bebida2').style.display = "none"
-    document.getElementById('select-bebida3').style.display = "none"
-    y = 1
 
-    bebida = 'Coquinha Gelada'
-    precoBebida = 4.90
+    if (varia === true) {
 
-    mudaBotao()
-    calculaPreco()
+        document.getElementById('bebida1').style.border = "3px solid #32B72F"
+        document.getElementById('bebida2').style.border = "3px solid white"
+        document.getElementById('bebida3').style.border = "3px solid white"
+        document.getElementById('select-bebida1').style.display = "initial"
+        document.getElementById('select-bebida2').style.display = "none"
+        document.getElementById('select-bebida3').style.display = "none"
+        y = 1
+
+        bebida = 'Coquinha'
+        precoBebida = 4.90
+
+        mudaBotao()
+        calculaPreco()
+
+    }
+
 }
 
 function selecionaBebida2 () {
-    document.getElementById('bebida1').style.border = "3px solid white"
-    document.getElementById('bebida2').style.border = "3px solid #32B72F"
-    document.getElementById('bebida3').style.border = "3px solid white"
-    document.getElementById('select-bebida1').style.display = "none"
-    document.getElementById('select-bebida2').style.display = "initial"
-    document.getElementById('select-bebida3').style.display = "none"
-    y = 1
 
-    bebida = 'Coquinha Gelada'
-    precoBebida = 4.90
+    if (varia === true) {
 
-    mudaBotao()
-    calculaPreco()
+        document.getElementById('bebida1').style.border = "3px solid white"
+        document.getElementById('bebida2').style.border = "3px solid #32B72F"
+        document.getElementById('bebida3').style.border = "3px solid white"
+        document.getElementById('select-bebida1').style.display = "none"
+        document.getElementById('select-bebida2').style.display = "initial"
+        document.getElementById('select-bebida3').style.display = "none"
+        y = 1
+
+        bebida = 'Coquinha Zero'
+        precoBebida = 5.90
+
+        mudaBotao()
+        calculaPreco()
+
+    }
 }
 
 function selecionaBebida3 () {
-    document.getElementById('bebida1').style.border = "3px solid white"
-    document.getElementById('bebida2').style.border = "3px solid white"
-    document.getElementById('bebida3').style.border = "3px solid #32B72F"
-    document.getElementById('select-bebida1').style.display = "none"
-    document.getElementById('select-bebida2').style.display = "none"
-    document.getElementById('select-bebida3').style.display = "initial"
-    y = 1
 
-    bebida = 'Coquinha Gelada'
-    precoBebida = 4.90
+    if (varia === true) {
 
-    mudaBotao()
-    calculaPreco()
+        document.getElementById('bebida1').style.border = "3px solid white"
+        document.getElementById('bebida2').style.border = "3px solid white"
+        document.getElementById('bebida3').style.border = "3px solid #32B72F"
+        document.getElementById('select-bebida1').style.display = "none"
+        document.getElementById('select-bebida2').style.display = "none"
+        document.getElementById('select-bebida3').style.display = "initial"
+        y = 1
+
+        bebida = 'Coquinha Gelada'
+        precoBebida = 5.90
+
+        mudaBotao()
+        calculaPreco()
+
+    }
+    
 }
 
 function selecionaSobremesa1 () {
-    document.getElementById('sobremesa1').style.border = "3px solid #32B72F"
-    document.getElementById('sobremesa2').style.border = "3px solid white"
-    document.getElementById('sobremesa3').style.border = "3px solid white"
-    document.getElementById('select-sobremesa1').style.display = "initial"
-    document.getElementById('select-sobremesa2').style.display = "none"
-    document.getElementById('select-sobremesa3').style.display = "none"
-    z = 1
 
-    sobremesa = 'Pudim'
-    precoSobremesa = 7.90
+    if (varia === true) {
 
-    mudaBotao()
-    calculaPreco()
+        document.getElementById('sobremesa1').style.border = "3px solid #32B72F"
+        document.getElementById('sobremesa2').style.border = "3px solid white"
+        document.getElementById('sobremesa3').style.border = "3px solid white"
+        document.getElementById('select-sobremesa1').style.display = "initial"
+        document.getElementById('select-sobremesa2').style.display = "none"
+        document.getElementById('select-sobremesa3').style.display = "none"
+        z = 1
+
+        sobremesa = 'Pudim P'
+        precoSobremesa = 7.90
+
+        mudaBotao()
+        calculaPreco()
+
+    }
+    
 }
 
 function selecionaSobremesa2 () {
-    document.getElementById('sobremesa1').style.border = "3px solid white"
-    document.getElementById('sobremesa2').style.border = "3px solid #32B72F"
-    document.getElementById('sobremesa3').style.border = "3px solid white"
-    document.getElementById('select-sobremesa1').style.display = "none"
-    document.getElementById('select-sobremesa2').style.display = "initial"
-    document.getElementById('select-sobremesa3').style.display = "none"
-    z = 1
 
-    sobremesa = 'Pudim'
-    precoSobremesa = 7.90
+    if (varia === true) {
 
-    mudaBotao()
-    calculaPreco()
+        document.getElementById('sobremesa1').style.border = "3px solid white"
+        document.getElementById('sobremesa2').style.border = "3px solid #32B72F"
+        document.getElementById('sobremesa3').style.border = "3px solid white"
+        document.getElementById('select-sobremesa1').style.display = "none"
+        document.getElementById('select-sobremesa2').style.display = "initial"
+        document.getElementById('select-sobremesa3').style.display = "none"
+        z = 1
+
+        sobremesa = 'Pudim M'
+        precoSobremesa = 10.90
+
+        mudaBotao()
+        calculaPreco()
+
+    }
+    
 }
 
 function selecionaSobremesa3 () {
-    document.getElementById('sobremesa1').style.border = "3px solid white"
-    document.getElementById('sobremesa2').style.border = "3px solid white"
-    document.getElementById('sobremesa3').style.border = "3px solid #32B72F"
-    document.getElementById('select-sobremesa1').style.display = "none"
-    document.getElementById('select-sobremesa2').style.display = "none"
-    document.getElementById('select-sobremesa3').style.display = "initial"
-    z = 1
 
-    sobremesa = 'Pudim'
-    precoSobremesa = 7.90
+    if (varia === true) {
 
-    mudaBotao()
-    calculaPreco()
+        document.getElementById('sobremesa1').style.border = "3px solid white"
+        document.getElementById('sobremesa2').style.border = "3px solid white"
+        document.getElementById('sobremesa3').style.border = "3px solid #32B72F"
+        document.getElementById('select-sobremesa1').style.display = "none"
+        document.getElementById('select-sobremesa2').style.display = "none"
+        document.getElementById('select-sobremesa3').style.display = "initial"
+        z = 1
+
+        sobremesa = 'Pudim G'
+        precoSobremesa = 14.90
+
+        mudaBotao()
+        calculaPreco()
+
+    }
+    
 }
 
 
